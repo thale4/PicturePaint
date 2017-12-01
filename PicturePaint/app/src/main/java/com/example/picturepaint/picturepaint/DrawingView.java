9fa9dd0ff60560a20ec1dcdf983d7bbb61eadac6
Trevor Hale <thale4@HARDING.EDU>
2017-12-01 16:28:42 -0600

    user can change the size of the brush

---
+++ PicturePaint/app/src/main/java/com/example/picturepaint/picturepaint/DrawingView.java
@@ -0,0 +1,110 @@
+package com.example.picturepaint.picturepaint;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.graphics.Bitmap;
+import android.graphics.Path;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+
+public class DrawingView extends android.support.v7.widget.AppCompatImageView
+{
+    //drawing path
+    private Path drawPath;
+    //drawing and canvas paint
+    private Paint drawPaint, canvasPaint;
+    //initial color
+    private int paintColor = Color.BLACK;
+    //canvas
+    private Canvas drawCanvas;
+    //canvas bitmap
+    private Bitmap canvasBitmap;
+
+    public DrawingView(Context context, AttributeSet attrs){
+        super(context, attrs);
+        setupDrawing();
+    }
+
+    private void setupDrawing(){
+        //get drawing area setup for interaction
+        drawPath = new Path();
+        drawPaint = new Paint();
+
+        drawPaint.setColor(paintColor);
+
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(20);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        //draw view
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        //view given size
+        super.onSizeChanged(w, h, oldw, oldh);
+        //canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        canvasBitmap = canvasBitmap.copy(Bitmap.Config.ARGB_8888, true);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //detect user touch
+        float touchX = event.getX();
+        float touchY = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                drawPath.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                drawPath.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+                break;
+            default:
+                return false;
+        }
+        invalidate();
+        return true;
+    }
+
+    //update color
+    public void setColor(int newColor){
+        invalidate();
+        paintColor = newColor;
+        drawPaint.setColor(paintColor);
+    }
+
+    public void setBitmap(Bitmap b){
+        invalidate();
+        canvasBitmap = b;
+        canvasBitmap = canvasBitmap.copy(Bitmap.Config.ARGB_8888, true);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+    public void setSize(int size){
+        drawPaint.setStrokeWidth(size);
+        Log.d("size", "view size: "+size);
+    }
+}
--- PicturePaint/app/src/main/java/com/example/picturepaint/picturepaint/EditActivity.java
+++ PicturePaint/app/src/main/java/com/example/picturepaint/picturepaint/EditActivity.java
@@ -4,45 +4,72 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 
-public class EditActivity extends AppCompatActivity implements ColorPickerDialog.OnColorChangedListener{
+public class EditActivity extends AppCompatActivity implements ColorPickerDialog.OnColorChangedListener {
 
-    private ImageView mImageView;
+    private DrawingView mImageView;
     private String mCurrentPhotoPath;
     private SeekBar mBrushSize;
     private SeekBar mBrushTransparency;
     private int mCurrentColor;
+    private Bitmap mBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
 
-        mImageView = (ImageView)findViewById(R.id.image);
+        mImageView = (DrawingView)findViewById(R.id.image);
 
         Intent intent = getIntent();
         mCurrentPhotoPath = intent.getStringExtra("filename");
-        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath);
-        mImageView.setImageBitmap(bitmap);
-
+        mBitmap = BitmapFactory.decodeFile(mCurrentPhotoPath);
+        mImageView.setImageBitmap(mBitmap);
 
         // Display "UP" button
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
 
         mCurrentColor = Color.BLACK;
+        mImageView.setColor(mCurrentColor);
+        mImageView.setBitmap(mBitmap);
+
 
         mBrushSize = (SeekBar)findViewById(R.id.brushSize);
         mBrushSize.setProgress(25);
+        mBrushSize.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+                                                  @Override
+                                                  public void onStopTrackingTouch(SeekBar seekBar) {
+                                                      if(seekBar.getId() == R.id.brushSize){
+                                                          mImageView.setSize(seekBar.getProgress());
+                                                          Log.d("size", "progress size: "+seekBar.getProgress());
+                                                      }
+                                                  }
+
+                                                  @Override
+                                                  public void onStartTrackingTouch(SeekBar seekBar) {
+                                                  }
+
+                                                  @Override
+                                                  public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                                                  }
+                                              });
+        mImageView.setSize(mBrushSize.getProgress());
+
         mBrushTransparency = (SeekBar)findViewById(R.id.brushTransparency);
         mBrushTransparency.setProgress(0);
     }
@@ -57,6 +84,7 @@ public class EditActivity extends AppCompatActivity implements ColorPickerDialog
         mCurrentColor = color;
         Button colorButton = (Button) findViewById(R.id.brushColorButton);
         colorButton.setBackgroundColor(color);
+        mImageView.setColor(color);
     }
 
     public void showHelp(View view) {
@@ -70,5 +98,8 @@ public class EditActivity extends AppCompatActivity implements ColorPickerDialog
                     }
                 });
         alertDialog.show();
+    }
+
+    public void saveImage(View view) {
     }
 }
