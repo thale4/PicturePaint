0277a75b12a4e73e802df04793fed94be9719d7c
Trevor Hale <thale4@HARDING.EDU>
2017-12-01 14:55:37 -0600

    can now choose a color
    used android's colorpickerdialog class

--- PicturePaint/app/src/main/java/com/example/picturepaint/picturepaint/EditActivity.java
+++ PicturePaint/app/src/main/java/com/example/picturepaint/picturepaint/EditActivity.java
@@ -1,21 +1,26 @@
 package com.example.picturepaint.picturepaint;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 
-public class EditActivity extends AppCompatActivity {
+public class EditActivity extends AppCompatActivity implements ColorPickerDialog.OnColorChangedListener{
 
     private ImageView mImageView;
     private String mCurrentPhotoPath;
     private SeekBar brushSize;
     private SeekBar brushTransparency;
+    private int mCurrentColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +38,32 @@ public class EditActivity extends AppCompatActivity {
         // Display "UP" button
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        mCurrentColor = Color.BLACK;
+    }
+
+    protected void pickColor(View view)
+    {
+        ColorPickerDialog colorPickerDialog = new ColorPickerDialog(this, this, mCurrentColor);
+        colorPickerDialog.show();
+    }
+
+    public void colorChanged(int color){
+        mCurrentColor = color;
+        Button colorButton = (Button) findViewById(R.id.brushColorButton);
+        colorButton.setBackgroundColor(color);
+    }
+
+    public void showHelp(View view) {
+        AlertDialog alertDialog = new AlertDialog.Builder(EditActivity.this).create();
+        alertDialog.setTitle("HELP");
+        alertDialog.setMessage("To choose a color, pick a color on the wheel and then press the color in the center");
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
     }
 }
